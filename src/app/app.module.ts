1159a81350ea4fd8d8d8cb58a5ce2b6a4ba01ae5
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-08-23 18:33:00 -0300

    add project

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { DEFAULT_CURRENCY_CODE } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+registerLocaleData(localePt, 'pt');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
